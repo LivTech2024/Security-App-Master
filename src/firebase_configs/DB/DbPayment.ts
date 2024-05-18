a4ab922b9d159277a93237cc7804d0a5308a487e
Yuvraj210103 <ys146228@gmail.com>
2024-05-18 10:18:21 +0530

    fix: errors fixed

--- src/firebase_configs/DB/DbPayment.ts
+++ src/firebase_configs/DB/DbPayment.ts
@@ -83,6 +83,7 @@ class DbPayment {
         InvoiceClientName: data.InvoiceClientName,
         InvoiceClientPhone: data.InvoiceClientPhone,
         InvoiceClientAddress: data.InvoiceClientAddress || null,
+        InvoiceLocationId: data.InvoiceLocationId,
         InvoiceNumber: data.InvoiceNumber,
         InvoiceDate: removeTimeFromDate(
           data.InvoiceDate
@@ -149,6 +150,7 @@ class DbPayment {
         InvoiceClientName: data.InvoiceClientName,
         InvoiceClientPhone: data.InvoiceClientPhone,
         InvoiceClientAddress: data.InvoiceClientAddress || null,
+        InvoiceLocationId: data.InvoiceLocationId,
         InvoiceNumber: data.InvoiceNumber,
         InvoiceDate: removeTimeFromDate(
           data.InvoiceDate
