f094d72b41b0b38cef1f398661c31079716be999
Yuvraj210103 <ys146228@gmail.com>
2024-06-07 17:35:53 +0530

    feat: added more details in callout

--- src/component/callout/modal/CreateCalloutModal.tsx
+++ src/component/callout/modal/CreateCalloutModal.tsx
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Dialog from '../../../common/Dialog';
 import InputSelect from '../../../common/inputs/InputSelect';
 import useFetchLocations from '../../../hooks/fetch/useFetchLocations';
 import { ILocationsCollection } from '../../../@types/database';
+import useFetchEmployees from '../../../hooks/fetch/useFetchEmployees';
+import { MdClose } from 'react-icons/md';
 
 const CreateCalloutModal = ({
   opened,
@@ -18,8 +20,37 @@ const CreateCalloutModal = ({
     searchQuery: locationSearchQuery,
   });
 
+  const [empSearchQuery, setEmpSearchQuery] = useState('');
+
+  const { data: employees } = useFetchEmployees({
+    limit: 5,
+    searchQuery: empSearchQuery,
+  });
+
   const [selectedLocation, setSelectedLocation] =
     useState<ILocationsCollection | null>(null);
+
+  const [selectedEmployee, setSelectedEmployee] = useState('');
+
+  const [assignedEmpsId, setAssignedEmpsId] = useState<
+    { id: string; name: string }[]
+  >([]);
+
+  useEffect(() => {
+    if (selectedEmployee && selectedEmployee.length > 0) {
+      setAssignedEmpsId((prev) => {
+        if (prev.find((rec) => rec.id === selectedEmployee)) {
+          return prev;
+        }
+        const empName = employees.find(
+          (emp) => emp.EmployeeId === selectedEmployee
+        )?.EmployeeName;
+        return [...prev, { id: selectedEmployee, name: empName || '' }];
+      });
+    }
+    setSelectedEmployee('');
+    setEmpSearchQuery('');
+  }, [selectedEmployee]);
 
   return (
     <Dialog
@@ -27,8 +58,9 @@ const CreateCalloutModal = ({
       setOpened={setOpened}
       title="Create callout"
       isFormModal
+      size="60%"
     >
-      <div className="grid grid-cols-2">
+      <div className="grid grid-cols-2 gap-4">
         <InputSelect
           label="Select Location"
           searchable
@@ -51,6 +83,43 @@ const CreateCalloutModal = ({
             return { label: res.LocationName, value: res.LocationId };
           })}
         />
+
+        <InputSelect
+          className="mx-0 w-full"
+          label="Select employee"
+          data={employees.map((res) => {
+            return { label: res.EmployeeName, value: res.EmployeeId };
+          })}
+          value={selectedEmployee}
+          searchValue={empSearchQuery}
+          onSearchChange={setEmpSearchQuery}
+          onChange={(e) => setSelectedEmployee(e as string)}
+          searchable
+        />
+
+        <div className="flex flex-col col-span-2">
+          <span className="font-semibold">Assigned Employees:</span>
+
+          <div className="flex flex-wrap gap-4  max-h-[120px] overflow-scroll remove-vertical-scrollbar py-2">
+            {assignedEmpsId.map((rec, idx) => {
+              return (
+                <span className="bg-onHoverBg p-2 rounded flex justify-between gap-4 w-fit">
+                  <span>
+                    {idx + 1}. {rec.name}
+                  </span>
+                  <MdClose
+                    className="text-textPrimaryRed text-xl cursor-pointer"
+                    onClick={() =>
+                      setAssignedEmpsId((prev) =>
+                        prev.filter((i) => i.id !== rec.id)
+                      )
+                    }
+                  />
+                </span>
+              );
+            })}
+          </div>
+        </div>
       </div>
     </Dialog>
   );
