36c743a6e58d914aec30e3442f472b8ba23687cc
Yuvraj210103 <ys146228@gmail.com>
2024-06-09 15:42:53 +0530

    refactor: footer refactored

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,14 +1,10 @@
-import { useAuthState } from '../store';
-
 const Footer = () => {
-  const { company } = useAuthState();
   return (
     <footer className="flex w-full flex-col items-center ">
       <div className="flex w-full  flex-col items-center bg-gray-200 px-4 lg:px-36">
         <div className="flex w-full max-w-[1280px] flex-col py-4">
           <h2 className="text-center text-xs">
-            &copy;{new Date().getFullYear()} {company?.CompanyName}. All rights
-            reserved.
+            &copy;{new Date().getFullYear()} Tacttik. All rights reserved.
           </h2>
         </div>
       </div>
