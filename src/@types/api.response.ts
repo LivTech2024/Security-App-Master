f7a42436f1f3a7da931096477f7376d39bf90942
Yuvraj210103 <ys146228@gmail.com>
2024-08-02 12:25:20 +0530

    refactor

---
+++ src/@types/api.response.ts
@@ -0,0 +1,11 @@
+export interface IIpDetails {
+  device: {
+    os_name: string;
+    os_version: string;
+    client: string;
+    client_version: string;
+    device_id: string;
+    device_manufacturer: string;
+    device_model: string;
+  };
+}
